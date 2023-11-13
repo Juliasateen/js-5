42734c7fb879531e2effc4243936eee1fd6a29e4
Juliasateen <julia-kuchinskaya@yandex.ru>
2023-11-13 07:44:26 +0300

    init: init homework js-5

---
+++ scripts.js
@@ -0,0 +1,66 @@
+"use strict";
+
+/** task 1 **/
+
+{
+    // function calcNumber(a, b, c) {
+    //     return (a - b) / c;
+    // }
+    // console.log(calcNumber(48, 4, 2));
+
+    // let calcNumber = (a, b, c) => {return (a - b) / c;}
+    // console.log(calcNumber(8, 3, 2));
+} 
+
+/** task 2 **/
+
+{
+    // function getCubeSquare(number) {
+    //     console.log(Math.pow(number, 3), 'число в кубе');
+    //     console.log(Math.pow(number, 2), 'число в квадрате');
+    // }
+
+    // getCubeSquare(47);
+}
+
+/** task 3 **/
+
+{
+    // function getMinMax(a, b) {
+    //     return a < b ? "a - наименьшее число" : 'b - наибольшее число';
+    // }
+
+    // console.log(getMinMax(14, 55));
+
+}
+
+/** task 4 **/
+
+{
+    // function createArray(a, b) {
+    //     let array = [];
+
+    //     for (let i = a; i <=b; i++) {
+    //         array.push(i);
+    //     }
+
+    //     // console.log(array);
+    //     return array;
+        
+    // }
+    
+    // let outputArray = createArray(+prompt('Введите первый элемент массива'), +prompt('Введеите последний элемент массива'));
+    // console.log(outputArray);
+
+}
+
+/** task 5 **/
+
+{
+    let isEven = (a) => {return a % 2 === 0 ? true : false;}
+    console.log(isEven(5));
+
+    
+
+}
+
